000b11d918785cfa006e571792bbc714f97e9434
Xin Tan <mactanxin@gmail.com>
2020-04-18 11:05:43 +0800

    feat: single hexagon component

---
+++ src/components/Hexagon.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hexagon"></div>
+</template>
+
+<script>
+</script>
+
+<style>
+.hexagon{
+  position: relative;
+  height: 110px;
+  width: 100px;
+  background: #111;
+  margin: 1px;
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+  transition: 2s;
+}
+</style>
