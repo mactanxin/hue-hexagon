b8c2dbeda995c882c63adfdbfdcffdbb02af4c7e
Xin Tan <mactanxin@gmail.com>
2020-04-18 11:07:54 +0800

    feat: hover before and after effect

--- src/components/Hexagon.vue
+++ src/components/Hexagon.vue
@@ -15,4 +15,19 @@
   clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
   transition: 2s;
 }
+.hexagon:before{
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 50%;
+  height: 100%;
+  pointer-events: none;
+  background: rgba(255, 255, 255, 0.02);
+}
+.hexagon:hover{
+  transition: 0s;
+  background: #0f0;
+  cursor: alias;
+}
 </style>
