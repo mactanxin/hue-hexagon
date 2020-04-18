f7be8ddb3e5930953707a5ffa6476e39a6517f3d
Xin Tan <mactanxin@gmail.com>
2020-04-18 11:08:39 +0800

    feat: Hexagon component imported in main app

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hexagon></hexagon>
+    <hexagon></hexagon>
+    <hexagon></hexagon>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Hexagon from './components/Hexagon.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Hexagon
   }
 }
 </script>
